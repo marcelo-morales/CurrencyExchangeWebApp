66749a0eec15ddb3148a2e927e3b063a09994624
marcelomorales <lmoral10@jhu.edu>
2020-07-27 12:49:38 -0400

    conversion works, added error handling if user chooses a currency that is not one of the choices, need to convert to JSON and then add sparkjava parameter functionality

--- main/java/org/codelab/currencyCalculator/services/CurrencyService.java
+++ main/java/org/codelab/currencyCalculator/services/CurrencyService.java
@@ -1,12 +1,14 @@
 package org.codelab.currencyCalculator.services;
 
 import org.codelab.currencyCalculator.model.CurrencyExchange;
+import org.codelab.currencyCalculator.services.data.CurrencyDAO;
+import org.codelab.currencyCalculator.services.data.SqlCurrencyDAO;
 import spark.Spark;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 
 import static org.codelab.currencyCalculator.services.DatabaseService.getAllInformation;
@@ -14,20 +16,65 @@ import static org.codelab.currencyCalculator.services.DatabaseService.getAllInfo
 public class CurrencyService {
 
     //DB service
-    private DatabaseService databaseService;
+    private final DatabaseService databaseService;
 
     //Data Provider
-    private DataProvider dataProvider;
+    private final DataProvider dataProvider;
 
     public Map<String, BigDecimal> mapOfRates;
 
     //constructor
-    public CurrencyService(DatabaseService databaseService, DataProvider dataProvider) {
+    public CurrencyService(DatabaseService databaseService, DataProvider dataProvider, Map<String, BigDecimal> mapOfRates) {
         this.databaseService = databaseService;
         this.dataProvider = dataProvider;
+        this.mapOfRates = mapOfRates;
     }
 
-    public static void main(String [] args) throws Exception {
+    public Map<String, BigDecimal> getAndReturnRates() throws Exception {
+        List<CurrencyExchange> results = getAllInformation("USD", Arrays.asList("USD", "GBP", "AUD", "JPY", "GBP", "CAD", "PEN", "MXN", "ZAR", "RUB", "EUR"));
+        //ListIterator<CurrencyExchange> it = results.listIterator();
+        int index = 0;
+        System.out.println("This is the rates " + results.toString());
+        System.out.println(results.size());
+
+        while (index < results.size()) {
+            System.out.println("Marcelo");
+            CurrencyExchange current = results.get(index);
+            System.out.println("Marcelo 2");
+            BigDecimal rate = current.getExchangeRate();
+            String id = current.getCurrencyId();
+            System.out.println("Marcelo 3");
+            this.mapOfRates.put(id, rate);
+            System.out.println("Marcelo 4");
+            System.out.println("This is the result" + this.mapOfRates.toString());
+            ++index;
+
+            /*
+            //..CurrencyExchange next = (CurrencyExchange) it.next();
+            //System.out.println(next.toString());
+            BigDecimal rate = results.get(index).getExchangeRate();
+            String id = results.get(index).getCurrencyId();
+            System.out.println("Rate is " + rate + " id is " + id);
+
+
+            System.out.println("The corresponding rate is " + rate);
+            this.mapOfRates.put(id, rate);
+            System.out.println("HIIIIII");
+            System.out.println(this.mapOfRates.toString());
+            ++index;
+            */
+
+        }
+        return this.mapOfRates;
+    }
+
+
+    public BigDecimal doConversion(String currencyFrom, String currencyTo, BigDecimal inputAmount) throws Exception {
+        BigDecimal rateForConversion = this.mapOfRates.get(currencyTo);
+        return inputAmount.multiply(rateForConversion);
+    }
+
+    public static void main(String[] args) throws Exception {
         System.out.println("Please work");
         Spark.get("/convert", (req, res) -> "This is a check! If got here, it worked");
 
@@ -42,37 +89,32 @@ public class CurrencyService {
             return "{\"message\":\"Sorry for the error, please let us know how to fix this by contacting the owner(s) of this application\"}";
         });
 
+        try {
+            String sample_input_currency_Id = "USD";
+            String sample_output_currency_Id = "PEn";
+            BigDecimal sample_amount_to_convert = BigDecimal.valueOf(234.45);
 
-        List<CurrencyExchange> results = getAllInformation("USD", Arrays.asList("USD","GBP","AUD", "JPY", "GBP", "CAD", "PEN", "MXN", "ZAR", "RUB", "EUR"));
-        ListIterator<CurrencyExchange> it = results.listIterator();
-        int index = 0;
-        while (it.hasNext()) {
-            CurrencyExchange next = (CurrencyExchange)it.next();
-            System.out.println(next.toString());
-            BigDecimal rate = results.get(index).getExchangeRate();
-            System.out.println("The corresponding rate is " + rate);
-            ++index;
+
+            CurrencyDAO cDao = new SqlCurrencyDAO();
+            DatabaseService ds = new DatabaseService(cDao);
+            DataProvider dp = new OpenExchangeRatesDataProvider();
+            Map<String, BigDecimal> mapOfRates = new HashMap<>();
+
+            CurrencyService cs = new CurrencyService(ds, dp, mapOfRates);
+
+            Map<String, BigDecimal> list_of_all_rates = cs.getAndReturnRates();
+            System.out.println("The list of all rates is " + list_of_all_rates.toString());
+
+
+            BigDecimal answer_of_conversion = cs.doConversion(sample_input_currency_Id, sample_output_currency_Id, sample_amount_to_convert);
+            System.out.println("Final answer is " + answer_of_conversion);
+
+            //create JSON object as instructed
+            //Spark java with format, /convert, correspond to parameters
+        }
+        catch (NullPointerException error) {
+            System.out.println("Something wrong with user input. Please make sure to choose one of the provided currency choices");
         }
     }
-
-    public static Map<String, BigDecimal> updateAndReturnMapOfRates() throws Exception {
-        List<CurrencyExchange> results = getAllInformation("USD", Arrays.asList("USD","GBP","AUD", "JPY", "GBP", "CAD", "PEN", "MXN", "ZAR", "RUB", "EUR"));
-        ListIterator<CurrencyExchange> it = results.listIterator();
-        int index = 0;
-        while (it.hasNext()) {
-            CurrencyExchange next = (CurrencyExchange)it.next();
-            BigDecimal rate = results.get(index).getExchangeRate();
-            String currency_id = results.get(index).getCurrencyId();
-            ++index;
-
-        }
-    }
-
-    public static BigDecimal doConversion(String currencyFrom, String currencyTo, BigDecimal inputAmount, BigDecimal rateForConversion) throws Exception {
-
-        return inputAmount.multiply(rateForConversion);
-    }
-
-
 
 }
