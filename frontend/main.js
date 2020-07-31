b1086b6b85930ae136fbe5cbfcc177660137f495
marcelomorales <lmoral10@jhu.edu>
2020-07-31 08:58:35 -0400

    merge

--- frontend/main.js
+++ frontend/main.js
@@ -4,7 +4,7 @@ const addCurrencyBtn = document.querySelector(".add-currency-btn");
 const addCurrencyList = document.querySelector(".add-currency-list");
 const currenciesList = document.querySelector(".currencies");
 
-const dataURL = "http://localhost:4567/convert/USD/toCurrency/EUR/1000";
+const dataURL = "https://api.exchangeratesapi.io/latest";
 
 const initiallyDisplayedCurrencies = ["USD", "EUR", "GBP", "JPY", "RUB"];
 let baseCurrency;
